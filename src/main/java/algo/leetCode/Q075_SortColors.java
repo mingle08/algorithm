d392983a7cb5688f8945a7d19beb2f89920bbacf
mingle08 <mingle08@163.com>
2018-11-27 13:42:13 +0800

    Q073-75

---
+++ src/main/java/algo/leetCode/Q075_SortColors.java
@@ -0,0 +1,37 @@
+package algo.leetCode;
+
+public class Q075_SortColors {
+
+    public static void main(String[] args) {
+        Q075_SortColors solution = new Q075_SortColors();
+        int[] arr = {0, 0, 0, 1, 1, 1, 2, 1, 0, 2, 1, 2, 2, 2};
+        solution.setColors(arr);
+
+        solution.printArr(arr);
+    }
+    
+    public void setColors(int[] nums){
+        int j = 0, k = nums.length - 1;
+        for (int i = 0; i <= k; i++) {
+            // 遇到0和前面的交换
+            if (nums[i] == 0 && j < nums.length-1)
+                swap(nums, i, ++j);
+            // 遇到2和后面的交换
+            else if (nums[i] == 2)
+                swap(nums, i--, k--);
+        }
+    }
+    
+    public void swap(int[] arr, int x, int y){
+        int tmp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = tmp;
+    }
+    
+    public void printArr(int[] num){
+        for(int i = 0; i < num.length; i++){
+            System.out.print(num[i] + " ");
+        }
+    }
+
+}
