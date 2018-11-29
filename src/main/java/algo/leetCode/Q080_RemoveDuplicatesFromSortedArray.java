9d289839cab145ffb12d0569e9b1fb6da4d13ef2
mingle08 <mingle08@163.com>
2018-11-29 23:46:03 +0800

    Q081

--- src/main/java/algo/leetCode/Q080_RemoveDuplicatesFromSortedArray.java
+++ src/main/java/algo/leetCode/Q080_RemoveDuplicatesFromSortedArray.java
@@ -3,32 +3,26 @@ package algo.leetCode;
 public class Q080_RemoveDuplicatesFromSortedArray {
 
     public static void main(String[] args) {
-        Q080_RemoveDuplicatesFromSortedArray solution = new Q080_RemoveDuplicatesFromSortedArray();
+//        Q080_RemoveDuplicatesFromSortedArray solution = new Q080_RemoveDuplicatesFromSortedArray();
         int[] arr = {1,1,1,2,2,3};
-        int n = solution.removeDuplicates(arr, 2);
+        int n = removeDuplicates(arr, 2);
         System.out.println(n);
 
     }
     
-    private int removeDuplicates(int[] nums, int k) {
-        int repeat= k; 
-        if(nums.length<=repeat)
-            return nums.length; 
-        int len=1; 
-        int cnt=1; 
-        for(int i=1;i<nums.length;i++){
-            if(nums[i] != nums[len-1]){ 
-                cnt=1;
-                nums[len++]=nums[i]; 
-            } else{ 
-                cnt++; 
-                if(cnt>repeat){ 
-                    continue; 
-                } else{ 
-                    nums[len++]=nums[i]; 
-                } 
-            } 
-        } 
+    private static int removeDuplicates(int[] nums, int k) {
+        //special case
+        if (nums == null) return 0;
+        //base case;
+        if (nums.length <= 2) return nums.length;
+        int len = 2;
+        //general case
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[len - 1] || nums[i] != nums[len - 2]) {
+                nums[len] = nums[i];
+                len++;
+            }
+        }
         return len;
       
     }
