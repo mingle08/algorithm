8bc55e0eb77dde931f14d5a110ffce271a7d3406
mingle08 <mingle08@163.com>
2018-11-30 11:54:46 +0800

    Update Q082

--- src/main/java/algo/leetCode/Q082_RemoveDuplicatesFromSortedListII.java
+++ src/main/java/algo/leetCode/Q082_RemoveDuplicatesFromSortedListII.java
@@ -1,7 +1,6 @@
 package algo.leetCode;
 
 public class Q082_RemoveDuplicatesFromSortedListII {
-
     public static void main(String[] args) {
         Q082_RemoveDuplicatesFromSortedListII solution = new Q082_RemoveDuplicatesFromSortedListII();
         ListNode node1 = new ListNode(1);
@@ -17,29 +16,37 @@ public class Q082_RemoveDuplicatesFromSortedListII {
         node4.next = node5;
         node5.next = node6;
         node6.next = node7;
-        
+
         ListNode newNode = solution.deleteDuplicates(node1);
-        
+
         System.out.println(newNode.val);
 
     }
-    
+
     public ListNode deleteDuplicates(ListNode head) {
-        if(head == null) {
-            return null;
-        }
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        while(head.next != null && head.next.next != null) {
-            if(head.next.val == head.next.next.val) {
-                int val = head.next.val;
-                while (head.next != null && head.next.val == val) {
-                    head.next = head.next.next;
+        ListNode dummy = new ListNode(-1);
+        ListNode tmp = dummy;
+
+        while(head != null && head.next != null){
+            if (head.val == head.next.val){ //遇到相同的元素，整段删除
+                while(head.next != null && head.val == head.next.val){
+                    head = head.next;
                 }
+                /**
+                 * 第一次判断重复的1
+                 * 走到这一步，指针在第三个1，后面是2，因为这个1也是要删除的，所以head指针后移到2上
+                 * 第二次判断重复的3
+                 */
+
+                head = head.next;  // 第一次执行完成之后：head指向2;第二次执行完之后，head指向5
             } else {
-                head = head.next;
+                tmp.next = head;  // tmp的第二个节点是2；
+                tmp = tmp.next;   // tmp后移，变成2；
+                head = head.next; // head指向3
+
             }
         }
+        tmp.next = head;  // head指向5， tmp现在是2，next指向5
         return dummy.next;
     }
 
