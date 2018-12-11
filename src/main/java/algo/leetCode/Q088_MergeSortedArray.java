77a381847857dea0ae98d796a6e87ad31462b5f4
mingle08 <mingle08@163.com>
2018-12-11 11:39:23 +0800

    修改 归并处理剩余的逻辑

--- src/main/java/algo/leetCode/Q088_MergeSortedArray.java
+++ src/main/java/algo/leetCode/Q088_MergeSortedArray.java
@@ -8,18 +8,26 @@ public class Q088_MergeSortedArray {
         int j = n - 1;
 
         while (k >= 0) {
-            if (i < 0) {
-                nums1[k--] = nums2[j--];
-                continue;
-            }
-            if (j < 0) {
-                break;
-            }
-
+            
             // 大的数，从后往前放置
             if (nums1[i] > nums2[j]) {
                 nums1[k--] = nums1[i--];
             } else {
+                nums1[k--] = nums2[j--];
+            }
+
+            /**
+             * 当上面循环结束后，num1或者num2可能还有数据，
+             * 是或的关系，要么是num1还有剩余的，要么是nums2还有剩余的，不可能二者都有剩余
+             *
+             */
+            //可能是num1中还有数据
+            if (i > 0){
+                nums1[k--] = nums1[i--];
+            }
+
+            // 可能是nums2中还有数据
+            if (j > 0){
                 nums1[k--] = nums2[j--];
             }
         }
