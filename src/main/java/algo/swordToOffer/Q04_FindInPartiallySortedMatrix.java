a75ba373e830b194173190216db680860716e867
mingle <mingle08@163.com>
2018-12-14 09:31:31 +0800

    剑指offer 面试4
    二维数组，每行都是从左到右递增的，第列是从上到下递增的。查找某数是否存在于二维数组中

---
+++ src/main/java/algo/swordToOffer/Q04_FindInPartiallySortedMatrix.java
@@ -0,0 +1,44 @@
+package algo.Sword;
+
+/**
+ * 二维数组：每一行的数是从左到右递增的，每一列的数是从上到下递增的
+ * 思路：每次选取右上角（或左下角）的元素
+ */
+public class Q04_FindInPartiallySortedMatrix {
+
+    public boolean find(int[][] matrix, int num){
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int row = 0;
+        int col = cols - 1;
+
+        while (row < rows && col >= 0){ // 因为row是递增，col是递减
+
+            // matrix[row][col] 是右上角的元素
+            if (matrix[row][col] > num){  // 1. 如果右上角的元素比目标大，删除右上角元素所在的这一列
+                col--;
+            } else if (matrix[row][col] < num){  // 2. 如果右上角的元素比目标小，删除右上角元素所在的这一行
+                row++;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args){
+        Q04_FindInPartiallySortedMatrix solution = new Q04_FindInPartiallySortedMatrix();
+
+        int[][] nums = {{1, 2, 8, 9},
+                        {2, 4, 9, 12},
+                        {4, 7, 10, 13},
+                        {6, 8, 11, 15}};
+
+        boolean flag = solution.find(nums, 7);
+        System.out.println(flag);
+
+
+    }
+}
