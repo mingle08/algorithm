d54349092a65bc93d080325d5b3bc93e1c53d5cb
mingle08 <mingle08@163.com>
2018-12-15 10:42:49 +0800

    Q002  two sum

---
+++ src/main/java/algo/leetCode/Q002_TwoSum.java
@@ -0,0 +1,35 @@
+package algo.leetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q002_TwoSum {
+
+    public int[] findTwoSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] index = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])){
+                index[0] = map.get(target - nums[i]);
+                index[1] = i;
+                break;
+            }
+
+            map.put(nums[i], i);
+
+        }
+
+        return index;
+
+    }
+
+    public static void main(String[] args){
+        Q002_TwoSum solution = new Q002_TwoSum();
+        int[] arr = {2, 4, 7, 11, 15};
+        int[] index = solution.findTwoSum(arr, 9);
+        System.out.println(index[0] + ", " + index[1]);
+
+    }
+
+
+}
