992781ac2d125231a210ae245ef1057a6da34350
mingle08 <mingle08@163.com>
2018-12-20 22:10:23 +0800

    leetcode Q117

---
+++ src/main/java/algo/leetCode/Q117_PopulatingNextRightPointersInEachNodeII.java
@@ -0,0 +1,58 @@
+package algo.leetCode;
+
+/**
+          1
+        /  \
+       2    3
+     / \    \
+    4   5    7
+
+
+           1 -> NULL
+         /  \
+        2 -> 3 -> NULL
+       / \    \
+      4-> 5 -> 7 -> NULL
+ */
+
+public class Q117_PopulatingNextRightPointersInEachNodeII {
+
+    public void connect(TreeLinkNode root) {
+        if (null == root) {
+            return;
+        }
+
+        TreeLinkNode cur = root.next;
+        TreeLinkNode p = null;
+
+        while (cur != null) { // find last right node (left or right)
+            if (cur.left != null) {
+                p = cur.left;
+                break;
+            }
+            if (cur.right != null) {
+                p = cur.right;
+                break;
+            }
+            cur = cur.next;
+        }
+
+        if (root.right != null) {
+            root.right.next = p;
+        }
+
+        if (root.left != null) {
+            root.left.next = root.right != null ? root.right : p;
+        }
+
+        connect(root.right); // from right to left
+        connect(root.left);
+    }
+
+    public class TreeLinkNode {
+        int val;
+        TreeLinkNode left, right, next;
+        TreeLinkNode(int x) { val = x; }
+    }
+
+}
