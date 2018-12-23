987e8c87772fe0b13f6ea6507a05346487a85619
mingle08 <mingle08@163.com>
2018-12-23 21:11:21 +0800

    leetcode 120  注意 new ArrayList<>(list)

--- src/main/java/algo/leetCode/Q120_Triangle.java
+++ src/main/java/algo/leetCode/Q120_Triangle.java
@@ -6,16 +6,16 @@ import java.util.List;
 
 public class Q120_Triangle {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int row=triangle.size();    //行数
-        if(row==0)
+        int row = triangle.size();    //行数
+        if(row == 0)
             return 0;
 
-        int[] res=new int[row+1];   //倒着求，求最后一行到第一行最小和，这样就可以用o(n)空间了
+        int[] res = new int[row+1];   //倒着求，求最后一行到第一行最小和，这样就可以用o(n)空间了
 
-        for(int i=row-1;i>=0;i--){
-            List<Integer> list=triangle.get(i);
-            for(int j=0;j<list.size();j++){
-                res[j]=(Math.min(res[j+1],res[j])+list.get(j));//最后一行的最小值就是当前数
+        for(int i = row - 1; i >= 0; i--){
+            List<Integer> list = triangle.get(i);
+            for(int j = 0;j < list.size(); j++){
+                res[j] = (Math.min(res[j+1],res[j]) + list.get(j));//最后一行的最小值就是当前数
             }
         }
 
@@ -63,5 +63,9 @@ public class Q120_Triangle {
         triangle.add(new ArrayList<>(list));
 
         System.out.println(triangle);
+
+        Q120_Triangle solution = new Q120_Triangle();
+        int min = solution.minimumTotal(triangle);
+        System.out.println(min);
     }
 }
