2582c4f7f03d48c650670cdce73ee9e0cc69867a
mingle08 <mingle08@163.com>
2018-12-23 17:35:00 +0800

    剑指offer   Eight Queens   Permutations

--- src/main/java/algo/swordToOffer/Q038_3_EightQueens.java
+++ src/main/java/algo/swordToOffer/Q038_3_EightQueens.java
@@ -3,12 +3,12 @@ package algo.swordToOffer;
 public class Q038_3_EightQueens {
 
     private static int total = 0;
-    private boolean check(int columnIndex[],int length){
+    private boolean check(int cols[],int length){
         for(int i=0;i<length;i++){
             for(int j=i+1;j<length;j++){
                 //是否在对角线上
-                if(i-j==columnIndex[i]-columnIndex[j]
-                        ||j-i==columnIndex[i]-columnIndex[j]){
+                if(i - j == cols[i] - cols[j]
+                        || j - i == cols[i] - cols[j]){
                     return false;
                 }
             }
@@ -17,39 +17,50 @@ public class Q038_3_EightQueens {
     }
 
     //全排列
-    void Permutation(int columnIndex[],int length,int index){
-        if(length == index){
-            if(check(columnIndex,length)){
+    private void Permutation(int cols[],int length,int index){
+        if(length - 1 == index){
+            if(check(cols,length)){
                 total++;
                 //打印
-                for(int i=0;i<length;i++){
-                    System.out.printf("%d ",columnIndex[i]);
+                for(int i=0; i<length; i++){
+                    System.out.printf("%d ",cols[i]);
                 }
                 System.out.printf("\n---------------------------\n");
             }
         }else{
             for(int i= index ;i<length;i++){
-                swap(columnIndex, index, i);
-                Permutation(columnIndex,length,index+1);
-                swap(columnIndex, index, i);
+                swap(cols, index, i);
+                Permutation(cols,length,index+1);
+                swap(cols, index, i);
             }
         }
     }
 
     private void swap(int[] arr, int x, int y){
+        if (x == y)
+            return;
+
         int temp = arr[x];
         arr[x] = arr[y];
         arr[y] = temp;
     }
 
 
-    private void eightQueen(){
+    public void eightQueen(){
         int queens =8;
-        int[] columnIndex = new int[queens];
-        for(int i=0;i<queens;i++){
-            columnIndex[i] = i;
+        int[] cols = new int[queens];
+        for(int i=0; i < queens; i++){
+            cols[i] = i;
         }
-        Permutation(columnIndex, queens ,0);
+        Permutation(cols, queens ,0);
+    }
+
+    public static void main(String[] args){
+        Q038_3_EightQueens solution = new Q038_3_EightQueens();
+
+        solution.eightQueen();
+
+        System.out.println(total);   // 92
     }
 
 }
