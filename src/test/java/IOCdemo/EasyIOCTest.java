eae68a77189bf9254383229cf3482343e37d0fb8
mingle08 <mingle08@163.com>
2019-01-05 23:13:31 +0800

    getFile()返回的是file名称

--- src/test/java/IOCdemo/EasyIOCTest.java
+++ src/test/java/IOCdemo/EasyIOCTest.java
@@ -5,6 +5,15 @@ import org.junit.Test;
 public class EasyIOCTest {
     @Test
     public void getBean() throws Exception {
+        /**
+         *   URL.java中的getFile()方法返回的是file名称
+         *  * @return  the file name of this {@code URL},
+         *  * or an empty string if one does not exist
+         *
+         *  public String getFile () {
+         *     return file;
+         *  }
+         */
         String location = EasyIOC.class.getClassLoader().getResource("ioc.xml").getFile();
         EasyIOC bf = new EasyIOC(location);
         Wheel wheel = (Wheel) bf.getBean("wheel");
