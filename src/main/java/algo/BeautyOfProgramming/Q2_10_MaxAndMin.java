51e8c3406a4e581d021f94b9d7fb83456352bc98
mingle08 <mingle08@163.com>
2019-01-19 18:06:23 +0800

    编程之美 2.10 数组的最大值和最小值； 数组的第二大的数

---
+++ src/main/java/algo/BeautyOfProgramming/Q2_10_MaxAndMin.java
@@ -0,0 +1,26 @@
+package algo.BeautyOfProgramming;
+
+public class Q2_10_MaxAndMin {
+
+    public int[] findMaxAndMin(int[] arr){
+        int[] res = new int[2];
+        res[0] = arr[0];
+        res[1] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > res[0])
+                res[0] = arr[i];
+
+            if (arr[i] < res[1])
+                res[1] = arr[i];
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args){
+        Q2_10_MaxAndMin solution = new Q2_10_MaxAndMin();
+        int[] arr = {5, 6, 8, 3, 7, 9};
+        int[] res = solution.findMaxAndMin(arr);
+        System.out.println("最大值=" + res[0] + ", 最小值=" + res[1]);
+    }
+}
