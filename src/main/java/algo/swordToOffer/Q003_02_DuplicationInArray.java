0661856878344ae5e793f2f80f3494143cb60e0a
mingle08 <mingle08@163.com>
2021-03-11 08:06:55 +0800

    优化Q003题目二

--- src/main/java/algo/swordToOffer/Q003_02_DuplicationInArray.java
+++ src/main/java/algo/swordToOffer/Q003_02_DuplicationInArray.java
@@ -1,22 +1,27 @@
 package algo.swordToOffer;
 
 /**
- * 利用二分的思想，将数组值1~n分割为1~mid的n1，mid+1 ~ n的n2;
+ * 题目二：不修改数组，找出重复的数字
+ * 在一个长度为n+1的数组里，所有数字都在1~n的范围内，所以数组中至少有一个数字是重复的，
+ * 请找出数组中任意一个重复的数字，但不能改变输入的数组。
+ * 例如，如果输入长度为8的数组{2,3,5,4,3,2,6,7}，那么 对应的输出是重复的数字2或者3.
+ * 
+ * 思路：利用二分的思想，将数组值1~n分割为1~mid的n1，mid+1 ~ n的n2;
  * 从头开始遍历一遍数组， 统计在1~mid范围内数组中值的个数count，
  * 如果count > mid,那么重复的数字就在该半个数组中(n1)，否者在n2；
  * 直到end = start,找到重复的。时间复杂度为O(nlogn);
  *
  */
 public class Q003_02_DuplicationInArray {
-    public int getDup(int[] nums, int length){
-        if(nums == null || length < 0){
+    public int getDup(int[] nums){
+        if(nums == null || nums.length == 0){
             return -1;
         }
         int start = 1;
-        int end = length - 1;
+        int end = nums.length - 1;
         while(end >= start){
             int mid = ((end - start) >> 1) + start;
-            int count = countRange(nums,length, start,mid);  // 统计在左一半的目标数量
+            int count = countRange(nums, start, mid);  // 统计在左一半的目标数量
             if(end == start){
                 if(count > 1){
                     return start;
@@ -33,12 +38,12 @@ public class Q003_02_DuplicationInArray {
         return -1;
     }
 
-    int countRange(int[] nums, int length, int start, int end){
+    int countRange(int[] nums, int start, int end){
         if(nums == null){
             return 0;
         }
         int count = 0;
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if(nums[i] >= start && nums[i] <= end){
                 ++count;
             }
@@ -50,7 +55,7 @@ public class Q003_02_DuplicationInArray {
     public static void main(String[] args){
         Q003_02_DuplicationInArray solution = new Q003_02_DuplicationInArray();
         int[] arr = {2,3,5,4,3,2,6,7};
-        int dup = solution.getDup(arr, 8);
+        int dup = solution.getDup(arr);
         System.out.println(dup);
     }
 }
