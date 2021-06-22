5d4f3b7d70675cfeb1139c67fb06ed14675b46db
mingle08 <mingle08@163.com>
2021-06-22 22:35:11 +0800

    jol版本号0.9与0.16的打印结果比较

--- src/main/java/jdk/JolUtil.java
+++ src/main/java/jdk/JolUtil.java
@@ -12,6 +12,17 @@ public class JolUtil {
     }
 
     /**
+     * 1，joc版本为0.9的打印结果：
+     * java.lang.Object object internals:
+     *  OFFSET  SIZE   TYPE DESCRIPTION                               VALUE
+     *       0     4        (object header)                           01 00 00 00 (00000001 00000000 00000000 00000000) (1)
+     *       4     4        (object header)                           00 00 00 00 (00000000 00000000 00000000 00000000) (0)
+     *       8     4        (object header)                           e5 01 00 f8 (11100101 00000001 00000000 11111000) (-134217243)
+     *      12     4        (loss due to the next object alignment)
+     * Instance size: 16 bytes
+     * Space losses: 0 bytes internal + 4 bytes external = 4 bytes total
+     *
+     * 2，joc版本为0.16的打印结果：
      * java.lang.Object object internals:
      * OFF  SZ   TYPE DESCRIPTION               VALUE
      *   0   8        (object header: mark)     0x0000000000000001 (non-biasable; age: 0)
