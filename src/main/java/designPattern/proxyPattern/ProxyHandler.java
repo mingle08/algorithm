1d09f3038a2ab56d32cb139f97c724b28ee4b844
mingle08 <mingle08@163.com>
2021-06-26 18:02:28 +0800

    动态代理 handler的invoke方法中添加自己的逻辑

--- src/main/java/designPattern/proxyPattern/ProxyHandler.java
+++ src/main/java/designPattern/proxyPattern/ProxyHandler.java
@@ -14,6 +14,8 @@ public class ProxyHandler implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // 可以加处理逻辑，比如加日志
+        log(method.getName());
         Object res = method.invoke(obj, args);
         return res;
     }
@@ -21,5 +23,10 @@ public class ProxyHandler implements InvocationHandler {
     // 生成代理类
     public Object getProxy() {
         return Proxy.newProxyInstance(this.getClass().getClassLoader(), obj.getClass().getInterfaces(), this);
+    }
+
+    // 加日志
+    private void log(String msg) {
+        System.out.println("执行了" + msg + "方法");
     }
 }
