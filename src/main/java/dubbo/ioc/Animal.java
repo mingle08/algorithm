0f8dd9e61fbce67f7989fff1dd3a776c6d122b29
huxiangming <hu@lkl123>
2021-09-23 18:22:13 +0800

    dubbo ioc 草稿

---
+++ src/main/java/dubbo/ioc/Animal.java
@@ -0,0 +1,13 @@
+package dubbo.ioc;
+
+import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.common.extension.Adaptive;
+import com.alibaba.dubbo.common.extension.SPI;
+
+@SPI
+public interface Animal {
+    public void speak();
+
+    @Adaptive("friend")
+    public void speak(URL url);
+}
