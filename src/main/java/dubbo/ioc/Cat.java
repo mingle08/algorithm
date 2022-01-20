644e4f5c4d3a1eb18609441e725596d678ccd27c
mingle08 <mingle08@163.com>
2022-01-20 20:43:11 +0800

    增加一个单词：black

--- src/main/java/dubbo/ioc/Cat.java
+++ src/main/java/dubbo/ioc/Cat.java
@@ -5,7 +5,7 @@ import com.alibaba.dubbo.common.URL;
 public class Cat implements Animal {
     @Override
     public void speak() {
-        System.out.println("I am a cat");
+        System.out.println("I am a black cat");
     }
 
     @Override
