9cda36a37044e972f3a2b621a84524aa4f38afcf
mingleCat <mingle08@163.com>
2022-05-23 01:04:08 +0800

    算法第4版 1.1.6 静态方法：素数，平方根，调和级数

---
+++ src/main/java/algorithm/algorithmsFourthEdition/A116_StaticMethod.java
@@ -0,0 +1,31 @@
+package algorithm.algorithmsFourthEdition;
+
+public class A116_StaticMethod {
+
+    // 判定一个数是否是素数
+    public static boolean isPrime(int N) {
+        if (N < 2) return false;
+        for (int i = 2; i * i <= N; i++)
+            if (N % i == 0) return false;
+        return true;
+    }
+
+    // 计算平方根（牛顿迭代法）
+    public static double sqrt(double c) {
+        if (c < 0) return Double.NaN;
+        double err = 1e-15;
+        double t = c;
+        while (Math.abs(t - c/t) > err * t)
+            t = (c/t + t) / 2.0;
+        return t;
+    }
+
+    // 计算调和级数
+    public static double H(int N) {
+        double sum = 0.0;
+        for (int i = 1; i <= N; i++) {
+            sum += 1.0 / i;
+        }
+        return sum;
+    }
+}
