080ea6461ef95269caf2bd9f50c88dcefff38a0d
mingleCat <mingle08@163.com>
2022-07-03 13:06:02 +0800

    leetcode 14 最大公共前缀

--- src/main/java/algorithm/leetCode/LeetCode014_LongestCommonPrefix.java
+++ src/main/java/algorithm/leetCode/LeetCode014_LongestCommonPrefix.java
@@ -19,6 +19,7 @@ public class LeetCode014_LongestCommonPrefix {
 //                }
 //            }
             while(strs[i].indexOf(prefix) != 0){// 等于0，才是前缀
+                // 每次都去掉最后一个字符
                 prefix = prefix.substring(0, prefix.length() - 1);
             }
 
@@ -29,9 +30,44 @@ public class LeetCode014_LongestCommonPrefix {
     public static void main(String[] args){
         String[] a = {"flower","flow","flight"};
         long startTime = System.nanoTime();
-        String pre = longestCommonPrefix(a);
+        String pre = longestCommonPrefix2(a);
         long endTime = System.nanoTime();
         System.out.println((endTime-startTime) + "ns");  // 纳秒
         System.out.println(pre);
+    }
+
+    public static String longestCommonPrefix2(String[] strs) {
+        int count = -1;
+        int minLen = Integer.MAX_VALUE;
+        for (int i = 0; i < strs.length; i++) {
+            if (minLen > strs[i].length()) {
+                minLen = strs[i].length();
+            }
+        }
+
+        // 第一个字符串
+        String str0 = strs[0];
+        boolean flag = true;
+        for (int i = 0; i < minLen; i++) {
+            // 第i个字符
+            char c = str0.charAt(i);
+            for (int k = 1; k < strs.length; k++) {
+                // 其他字符串的第i个字符
+                char d = strs[k].charAt(i);
+                if (c != d) {
+                    flag = false;
+                    // 终止内循环
+                    break;
+                }
+            }
+            if (!flag) {
+                // 终止外循环
+                break;
+            }
+            count++;
+        }
+
+        if (count == -1) return "";
+        return strs[0].substring(0, count + 1);
     }
 }
