1a7e01f59e532721683333da17a8c98ee9647245
mingleCat <mingle08@163.com>
2022-07-03 18:39:47 +0800

    leetcode 51,52 N皇后

--- src/main/java/algorithm/leetCode/LeetCode051_N_Queens.java
+++ src/main/java/algorithm/leetCode/LeetCode051_N_Queens.java
@@ -3,29 +3,37 @@ package algorithm.leetCode;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 按照国际象棋的规则，皇后可以攻击与之处在同一行或同一列或同一斜线上的棋子。
+ *
+ * n 皇后问题 研究的是如何将 n 个皇后放置在 n × n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ *
+ * 给你一个整数 n ，返回所有不同的 n 皇后问题 的解决方案。
+ *
+ * 每一种解法包含一个不同的 n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/n-queens
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class LeetCode051_N_Queens {
-
-    public static void main(String[] args){
-
-        LeetCode051_N_Queens solution = new LeetCode051_N_Queens();
-        List<List<String>> result = solution.solveNQueens(5);
-        System.out.println(result.size());
-    }
 
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> res = new ArrayList<List<String>>();
-        int[] queenList = new int[n]; //第i个位置存放的数表示row行时，Q的列
-        placeQueen(queenList, 0, n, res);//在第0行放Q
+        // 第i个位置存放的数表示row行，而值表示哪一列
+        int[] queen = new int[n];
+        //在第0行开始放Q
+        placeQueen(queen, 0, n, res);
         return res;
     }
-    private void placeQueen(int[] queenList, int row, int n, List<List<String>> res) {
+    private void placeQueen(int[] queen, int row, int n, List<List<String>> res) {
         //如果已经填满，就生成结果
         if (row == n) {
-            ArrayList<String> list = new ArrayList<String>();
+            ArrayList<String> list = new ArrayList<>();
             for (int i = 0; i < n; i++) {
                 String str = "";
                 for (int col = 0; col < n; col++){
-                    if(queenList[i] == col) {
+                    if(queen[i] == col) {
                         str += "Q";
                     } else {
                         str += ".";
@@ -35,27 +43,41 @@ public class LeetCode051_N_Queens {
             }
             res.add(list);
         }
-        for (int col = 0; col < n; col++) {//循环每一列
-            if (isValid(queenList, row, col)) { //如果在该列放入Q不冲突的话
-                queenList[row] = col;
-                placeQueen(queenList, row + 1, n, res);
+        //循环每一列
+        for (int col = 0; col < n; col++) {
+            //如果在该列放入Q不冲突的话
+            if (isValid(queen, row, col)) {
+                queen[row] = col;
+                // 递归
+                placeQueen(queen, row + 1, n, res);
             }
         }
     }
 
-    private boolean isValid(int[] queenList, int row, int col) {
+    private boolean isValid(int[] queen, int row, int col) {
         for (int i = 0; i < row; i++) {
-            int pos = queenList[i];
+            // i代表行，pos代表列
+            int pos = queen[i];
             if (pos == col) { //和新加入的Q处于同一列
                 return false;
             }
-            if (pos + row - i == col) { //在新加入的Q的右对角线上
+            int d = row - i;
+            //在新加入的Q的右对角线上
+            if (pos + d == col) {
                 return false;
             }
-            if (pos - row + i == col) { //在新加入的Q的左对角线上
+            //在新加入的Q的左对角线上
+            if (pos - d == col) {
                 return false;
             }
         }
         return true;
+    }
+
+    public static void main(String[] args){
+
+        LeetCode051_N_Queens solution = new LeetCode051_N_Queens();
+        List<List<String>> result = solution.solveNQueens(5);
+        System.out.println(result.size());
     }
 }
--- src/main/java/algorithm/leetCode/LeetCode052_N_QueensII.java
+++ src/main/java/algorithm/leetCode/LeetCode052_N_QueensII.java
@@ -1,52 +1,54 @@
 package algorithm.leetCode;
 
-
+/**
+ * n 皇后问题 研究的是如何将 n 个皇后放置在 n × n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * <p>
+ * 给你一个整数 n ，返回 n 皇后问题 不同的解决方案的数量。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/n-queens-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class LeetCode052_N_QueensII {
 
     public static int num = 0;  // 累计方案
     public static final int MAX_QUEEN = 8;
     public static int[] cols = new int[MAX_QUEEN];
 
-    public static void main(String[] args) {
-        LeetCode052_N_QueensII queen = new LeetCode052_N_QueensII();
-//        queen.getCount(0);
-        num = queen.totalNQueens(MAX_QUEEN);
-        System.out.println(num);
-
-    }
-
     /**
      * 方法1
      * 以列来放置，先放第0列，再放第1列
+     *
      * @description
      * @author
      * @date 2018年11月14日上午11:20:17
      */
-    public void getCount(int n){// n是从哪一列开始遍历填皇后
+    public void getCount(int n) {// n是从哪一列开始遍历填皇后
         boolean[] rows = new boolean[MAX_QUEEN];  // 记录每列每个方格是否可以放皇后
-        for(int i = 0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             rows[cols[i]] = true;  // true表示已经有皇后
-            int d = n - i;  // 对角线
+            // 横坐标距离
+            int d = n - i;
             // slash方向
-            if(cols[i] - d >= 0){
+            if (cols[i] - d >= 0) {
                 rows[cols[i] - d] = true;
             }
             // backslash方向
-            if(cols[i] + d <= MAX_QUEEN - 1){
+            if (cols[i] + d <= MAX_QUEEN - 1) {
                 rows[cols[i] + d] = true;
             }
         }
         // 到此知道了哪些位置不能放皇后
-        for(int i=0; i<MAX_QUEEN;i++){
-            if(rows[i]){
+        for (int i = 0; i < MAX_QUEEN; i++) {
+            if (rows[i]) {
                 // 不能放
                 continue;
             }
             cols[n] = i;
             // 下面可能还有其它合适位置
-            if(n < MAX_QUEEN - 1){
-                getCount(n+1);
-            }else{
+            if (n < MAX_QUEEN - 1) {
+                getCount(n + 1);
+            } else {
                 // 找到了完整的一套方案
                 num++;
                 printQueen();
@@ -55,13 +57,13 @@ public class LeetCode052_N_QueensII {
 
     }
 
-    private void printQueen(){
+    private void printQueen() {
         System.out.println("第" + num + "种方案！");
-        for(int i=0; i<MAX_QUEEN; i++){
-            for(int j=0; j<MAX_QUEEN; j++){
-                if(i == cols[j]){
+        for (int i = 0; i < MAX_QUEEN; i++) {
+            for (int j = 0; j < MAX_QUEEN; j++) {
+                if (i == cols[j]) {
                     System.out.print("Q ");
-                }else{
+                } else {
                     System.out.print("+ ");
                 }
             }
@@ -72,41 +74,43 @@ public class LeetCode052_N_QueensII {
 
     public int totalNQueens(int n) {
         // 第i个位置存放的数表示row行时，Q的列
-        int[] queenList = new int[n];
+        int[] queen = new int[n];
         // 从第0行开始放
-        placeQueen(queenList, 0, n);
+        placeQueen(queen, 0, n);
         return num;
     }
 
     /**
      * 方法2
      * 以行来放置，先放第0行，再放第1行
+     *
      * @description
      * @author
      * @date 2018年11月14日上午11:21:03
      */
-    private void placeQueen(int[] queenList, int row, int n) {
+    private void placeQueen(int[] queen, int row, int n) {
         // 如果已经填满，就生成结果
         if (row == n) {
             num++;
-            printQueen2(queenList);
+            printQueen2(queen);
         }
         // 按照行进行放置
         for (int col = 0; col < n; col++) {// 循环每一列
-            if (isValid(queenList, row, col)) { // 如果在该列放入Q不冲突的话
+            if (isValid(queen, row, col)) { // 如果在该列放入Q不冲突的话
                 // 没有回溯，因为没有修改原结果集
                 // 只是临时记录结果
-                queenList[row] = col;
-                placeQueen(queenList, row + 1, n);
+                queen[row] = col;
+                // 递归
+                placeQueen(queen, row + 1, n);
             }
         }
     }
 
-    private boolean isValid(int[] queenList, int row, int col) {
+    private boolean isValid(int[] queen, int row, int col) {
         for (int i = 0; i < row; i++) {
             int d = row - i;
             // pos为列
-            int pos = queenList[i];
+            int pos = queen[i];
             if (pos == col) { // 和新加入的Q处于同一列
                 return false;
             }
@@ -120,19 +124,27 @@ public class LeetCode052_N_QueensII {
         return true;
     }
 
-    private void printQueen2(int[] queenList){
+    private void printQueen2(int[] queen) {
         System.out.println("第" + num + "种方案！");
-        for(int i=0; i<MAX_QUEEN; i++){
-            for(int j=0; j<MAX_QUEEN; j++){
-                if(i == queenList[j]){
+        for (int i = 0; i < MAX_QUEEN; i++) {
+            for (int j = 0; j < MAX_QUEEN; j++) {
+                if (i == queen[j]) {
                     System.out.print("Q ");
-                }else{
+                } else {
                     System.out.print("+ ");
                 }
             }
             // 换行
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        LeetCode052_N_QueensII queen = new LeetCode052_N_QueensII();
+//        queen.getCount(0);
+        num = queen.totalNQueens(MAX_QUEEN);
+        System.out.println(num);
+
     }
 
 }
