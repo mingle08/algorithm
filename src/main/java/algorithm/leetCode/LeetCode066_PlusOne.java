918d3309d15e95642473e701bea363c322d7a4a8
mingleCat <mingle08@163.com>
2022-07-05 01:49:34 +0800

    leetcode 66 加1

--- src/main/java/algorithm/leetCode/LeetCode066_PlusOne.java
+++ src/main/java/algorithm/leetCode/LeetCode066_PlusOne.java
@@ -1,27 +1,28 @@
 package algorithm.leetCode;
 
 public class LeetCode066_PlusOne {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         LeetCode066_PlusOne solution = new LeetCode066_PlusOne();
-        int[] arr = {9,9,9};
+        int[] arr = {9, 8, 9};
         int[] res = solution.plusOne(arr);
-        System.out.println(res.length);
+        System.out.println(res);
     }
 
-    public int[] plusOne(int[] num){
+    public int[] plusOne(int[] digits) {
 
-        for (int i = num.length-1; i >= 0; i--) {
-            // num.length-1下标对应的是个位数，从个位数往十位数，百位数检查
-            if(num[i] == 9){
-                num[i] = 0;
-            }else{
-                num[i]++;
-                return num;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            // digits.length-1下标对应的是个位数，从个位数往十位数，百位数检查
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                // 比如 125，只需最后一位加上1，得到126，return
+                digits[i]++;
+                return digits;
             }
 
         }
-
-        int[] res = new int[num.length + 1];
+        // 全部都是9.如果有一位不是9，就会在上面的else分支加1并return
+        int[] res = new int[digits.length + 1];
         res[0] = 1;
         return res;
     }
