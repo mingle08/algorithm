ae1df955e2c2d07d0c1a1e14bdd66ef73a1a69ff
mingleCat <mingle08@163.com>
2022-07-04 19:07:19 +0800

    leetcode 49 字母异位词分类

--- src/main/java/algorithm/leetCode/LeetCode049_GroupAnagrams.java
+++ src/main/java/algorithm/leetCode/LeetCode049_GroupAnagrams.java
@@ -6,36 +6,47 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ *
+ * 字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
+ *
+ * 示例 1:
+ *
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * 示例 2:
+ *
+ * 输入: strs = [""]
+ * 输出: [[""]]
+ * 示例 3:
+ *
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/group-anagrams
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class LeetCode049_GroupAnagrams {
-
-    public static void main(String[] args) {
-        LeetCode049_GroupAnagrams solution = new LeetCode049_GroupAnagrams();
-        String[] arr = {"eat", "tea", "tan", "ate", "nat", "bat"};
-        List<List<String>> list = solution.groupAnagrams(arr);
-
-        System.out.println(list.size());
-
-    }
-
 
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> list = new ArrayList<List<String>>();
         int len = strs.length;
-        if(len<1) return list;
-        Map<String,List<String>> map = new HashMap<String,List<String>>();
-        String tmp = "";
-        for(int i=0;i<len;i++){
-            tmp = strs[i];
+        if (len < 1) return list;
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        String key = "";
+        for (String str : strs) {
             // 打散成char -> 排序  -> 字符串，比如eat，tea转成char，排序再转成String，都是aet，作为map的key
-            char[] arrayOfString = tmp.toCharArray();
-            Arrays.sort(arrayOfString);
-            tmp = new String(arrayOfString);
-            if(map.containsKey(tmp)){
-                map.get(tmp).add(strs[i]);
-            }else{
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+            key = new String(ch);
+            if (map.containsKey(key)) {
+                map.get(key).add(str);
+            } else {
                 List<String> item = new ArrayList<String>();
-                item.add(strs[i]);
-                map.put(tmp, item);
+                item.add(str);
+                map.put(key, item);
             }
         }
         for (List<String> value : map.values()) {
@@ -44,5 +55,12 @@ public class LeetCode049_GroupAnagrams {
         return list;
     }
 
+    public static void main(String[] args) {
+        LeetCode049_GroupAnagrams solution = new LeetCode049_GroupAnagrams();
+        String[] arr = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> list = solution.groupAnagrams(arr);
 
+        System.out.println(list.size());
+        System.out.println(list);
+    }
 }
