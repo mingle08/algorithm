dccfc45238d35deaee5feaccc0a737588c39b31d
mingleCat <mingle08@163.com>
2022-07-05 17:09:54 +0800

    leetcode 67 二进制求和

--- src/main/java/algorithm/leetCode/LeetCode067_AddBinary.java
+++ src/main/java/algorithm/leetCode/LeetCode067_AddBinary.java
@@ -1,6 +1,50 @@
 package algorithm.leetCode;
 
+/**
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+ *
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ *
+ * 示例 1:
+ *
+ * 输入: a = "11", b = "1"
+ * 输出: "100"
+ * 示例 2:
+ *
+ * 输入: a = "1010", b = "1011"
+ * 输出: "10101"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/add-binary
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class LeetCode067_AddBinary {
+
+    public String addBinary(String a, String b) {
+        // 使a是长串，b是短串
+        if (a.length() < b.length()) {
+            String temp = a;
+            a = b;
+            b = temp;
+        }
+        int carry = 0;
+        // 方便转变成字符串
+        char[] ch = a.toCharArray();
+        for (int m = a.length() - 1, n = b.length() - 1; m >= 0; m--, n--) {
+            int sum;
+            if (n >= 0) {
+                sum = (ch[m] - '0') + (b.charAt(n) - '0') + carry;
+            } else {
+                sum = (ch[m] - '0') + carry;
+            }
+            ch[m] = (sum % 2 == 0 ? '0' : '1');
+            // 进位
+            carry = sum / 2;
+        }
+        String res = new String(ch);
+        return carry > 0 ? "1" + res : res;
+    }
+
     public static void main(String[] args){
         LeetCode067_AddBinary solution = new LeetCode067_AddBinary();
         String s1 = "11";
@@ -8,28 +52,4 @@ public class LeetCode067_AddBinary {
         String res = solution.addBinary(s1,s2);
         System.out.println(res);
     }
-
-    public String addBinary(String a, String b) {
-        int lengthA = a.length();
-        int lengthB = b.length();
-        // carry 为初始进制位数
-        int val, tempA, tempB, carry = 0;
-        StringBuffer sb = new StringBuffer();
-        int maxLength = lengthA >= lengthB ? lengthA : lengthB;
-        for (int i = 0; i < maxLength; i++) {
-            // 从右边开始逐位取出字符串 a、b 的字符值 tempA 和 tempB，如果长度不足，则用0替代
-            tempA = lengthA > i ? a.charAt(lengthA - i - 1) - '0' : 0;
-            tempB = lengthB > i ? b.charAt(lengthB - i - 1) - '0' : 0;
-            val = tempA + tempB + carry;
-            carry = val / 2;
-            val = val % 2;
-            sb.append(val + "");
-        }
-        // 如果最高位有进位，则最高位还要加一位 1
-        if (carry == 1)
-            sb.append(carry);
-        // 翻转结果
-        return sb.reverse().toString();
-    }
-
 }
